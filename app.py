8ca46484774a1d86908b43e35edd6ce588259e5e
Ji-Min-Lee <ljm94318@gmail.com>
2020-12-19 22:53:46 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,52 @@
+import os
+from glob import glob
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from PIL import Image
+from numpy import asarray
+from model_loader import get_result
+
+app = Flask(__name__)
+app.add_url_rule('/instance', endpoint='instance', view_func=app.send_static_file)
+
+
+def inference(image):
+    image = Image.open('static/' + image)
+    data = asarray(image)
+    # print(data)
+    result = get_result(data)
+    print(result)
+    return result
+
+
+@app.route('/')
+def welcome():
+    return 'Welcome to AI School!'
+
+
+@app.route('/demo')
+def demo():
+    img_list = get_images()
+    return render_template("index.html", name="David", img_list=img_list, img_cnt=len(img_list), inference=inference)
+
+
+def get_images():
+    img_list = []
+    for img_f in glob("static/*"):
+        if 'ai.png' in img_f:
+            continue
+        img_list.append(img_f.split("\\")[-1])
+    return img_list
+
+
+@app.route("/redirect/image/save", methods=['POST'])
+def save_image():
+    if request.method == 'POST':
+        f = request.files['filename']
+        f.save(os.path.join(app.static_folder, secure_filename(f.filename)))
+    return redirect(url_for('demo'))
+
+
+if __name__ == "__main__":
+    app.run()
+    # inference("airplane1.png")
---
+++ model_loader.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+import numpy as np
+
+tf.flags.DEFINE_string("checkpoint_dir", "C:/Users/user/Desktop/aischool_beginner-master/aischool_beginner-master/tensorflow_basic/lecture5/runs/1608033392/checkpoints", "Checkpoint directory from training run")
+tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
+tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
+FLAGS = tf.flags.FLAGS
+
+checkpoint_file = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
+graph = tf.Graph()
+
+
+label = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
+
+
+def get_result(image):
+    with graph.as_default():
+        session_conf = tf.ConfigProto(
+            allow_soft_placement=FLAGS.allow_soft_placement,
+            log_device_placement=FLAGS.log_device_placement)
+        sess = tf.Session(config=session_conf)
+
+        with sess.as_default():
+            # Load the saved meta graph and restore variables
+            saver = tf.train.import_meta_graph("{}.meta".format(checkpoint_file))
+            saver.restore(sess, checkpoint_file)
+
+            # Get the placeholders from the graph by name
+            input_x = graph.get_operation_by_name("X").outputs[0]
+            dropout_keep_prob = graph.get_operation_by_name("keep_prob").outputs[0]
+
+            hypothesis = graph.get_operation_by_name("hypothesis").outputs[0]
+
+            img_hypothesis = sess.run(hypothesis, {input_x: [image], dropout_keep_prob: 1.0})
+            result = np.argmax(img_hypothesis[0])
+            return label[result]
+
